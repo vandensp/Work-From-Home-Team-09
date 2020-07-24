a8cb91f10fd16516ae87f1e3727882d4eda560df
kadeball <kadeball@iu.edu>
2020-07-23 23:47:45 -0400

    fixed questions

--- src/components/Mood.js
+++ src/components/Mood.js
@@ -23,6 +23,12 @@ export default function Mood() {
         padding: "10px"
     };
 
+    let qStyle = {
+        position: "relative",
+        left: "200px",
+        padding: "10px"
+    };
+
 
     return (
         <div>
@@ -32,14 +38,14 @@ export default function Mood() {
             <Heading/>
                 <h1 className="headingStyle">Mood Check-In</h1>
         <FormControl className="form" component="fieldset">
-            <FormLabel component="legend">On a scale of 1-10 (1 being not at all, 5 completely) how well do you think your superiors assisted you in reducing your work-related stress?
-            </FormLabel>
+            <FormLabel style={qStyle} component="legend">How much has work affected your stress levels in last week?</FormLabel>
+
             <RadioGroup aria-label="q1r" name="question1" value={value} onChange={handleChange}>
-                <FormControlLabel style={formStyle} value="absolutely not" control={<Radio />} label="1" />
-                <FormControlLabel style={formStyle} value="no" control={<Radio />} label="2" />
-                <FormControlLabel style={formStyle} value="no opinion" control={<Radio />} label="3" />
-                <FormControlLabel style={formStyle} value="yes" control={<Radio />} label="4" />
-                <FormControlLabel style={formStyle} value="absolutely" control={<Radio />} label="5" />
+                <FormControlLabel style={formStyle} value="absolutely not" control={<Radio />} label="Not at All" />
+                <FormControlLabel style={formStyle} value="no" control={<Radio />} label="A little" />
+                <FormControlLabel style={formStyle} value="no opinion" control={<Radio />} label="Quite a bit" />
+                <FormControlLabel style={formStyle} value="yes" control={<Radio />} label="More than usual" />
+                <FormControlLabel style={formStyle} value="absolutely" control={<Radio />} label="Completely" />
 
             </RadioGroup>
         </FormControl>
@@ -48,55 +54,131 @@ export default function Mood() {
 
                     <div className="form">
                 <FormControl    component="fieldset">
-                    <FormLabel component="legend">On a scale of 1-10 (1 being not at all, 10 meaning completely) how has work contributed to your stress levels?</FormLabel>
+                    <FormLabel style={qStyle} component="legend">How has work-related stress affected your productivity in the last week?</FormLabel>
                     <RadioGroup aria-label="q2" name="question2" value={value} onChange={handleChange}>
-                        <FormControlLabel style={formStyle} value="heck no" control={<Radio />} label="1" />
-                        <FormControlLabel style={formStyle} value="nope" control={<Radio />} label="2" />
-                        <FormControlLabel style={formStyle} value="maybe" control={<Radio />} label="3" />
-                        <FormControlLabel style={formStyle} value="yeah" control={<Radio />} label="4" />
-                        <FormControlLabel style={formStyle} value="heck yes" control={<Radio />} label="5" />
+                        <FormControlLabel style={formStyle} value="heck no" control={<Radio />} label="Not at all" />
+                        <FormControlLabel style={formStyle} value="nope" control={<Radio />} label="A little" />
+                        <FormControlLabel style={formStyle} value="maybe" control={<Radio />} label="Quite a bit" />
+                        <FormControlLabel style={formStyle} value="yeah" control={<Radio />} label="More than usual" />
+                        <FormControlLabel style={formStyle} value="heck yes" control={<Radio />} label="Completely" />
 
                     </RadioGroup>
                 </FormControl>
             </div>
 
             <FormControl className="form"  component="fieldset">
-                <FormLabel component="legend">On a scale of 1-10 (1 being not at all, 10 completely) how has work contributed to your stress levels?</FormLabel>
+                <FormLabel style={qStyle} component="legend">How happy are you with the work you completed this week?</FormLabel>
                 <RadioGroup aria-label="q2" name="question2" value={value} onChange={handleChange}>
-                    <FormControlLabel style={formStyle} value="heck no1" control={<Radio />} label="1" />
-                    <FormControlLabel style={formStyle} value="no1" control={<Radio />} label="2" />
-                    <FormControlLabel style={formStyle} value="maybe1" control={<Radio />} label="3" />
-                    <FormControlLabel style={formStyle} value="yes1" control={<Radio />} label="4" />
-                    <FormControlLabel style={formStyle} value="heck yes1" control={<Radio />} label="5" />
+                    <FormControlLabel style={formStyle} value="heck no1" control={<Radio />} label="Not at all" />
+                    <FormControlLabel style={formStyle} value="no1" control={<Radio />} label="A little" />
+                    <FormControlLabel style={formStyle} value="maybe1" control={<Radio />} label="Quite a bit" />
+                    <FormControlLabel style={formStyle} value="yes1" control={<Radio />} label="More than usual" />
+                    <FormControlLabel style={formStyle} value="heck yes1" control={<Radio />} label="Completely" />
 
                 </RadioGroup>
             </FormControl>
             </div>
 
             <FormControl className="form"  component="fieldset">
-                <FormLabel component="legend"> On a scale of 1-10 (1 being not at all, 10 completely) How has work related stress affect your productivity?</FormLabel>
+                <FormLabel style={qStyle} component="legend"> Would you say you struggled with staying motivated this week?</FormLabel>
                 <RadioGroup aria-label="q2" name="question2" value={value} onChange={handleChange}>
-                    <FormControlLabel style={formStyle} value="heck no2" control={<Radio />} label="1" />
-                    <FormControlLabel style={formStyle} value="no2" control={<Radio />} label="2" />
-                    <FormControlLabel style={formStyle} value="maybe2" control={<Radio />} label="3" />
-                    <FormControlLabel style={formStyle} value="yes2" control={<Radio />} label="4" />
-                    <FormControlLabel style={formStyle} value="heck yes2" control={<Radio />} label="5" />
+                    <FormControlLabel style={formStyle} value="heck no2" control={<Radio />} label="Not al all" />
+                    <FormControlLabel style={formStyle} value="no2" control={<Radio />} label="A little" />
+                    <FormControlLabel style={formStyle} value="maybe2" control={<Radio />} label="Quite a bit" />
+                    <FormControlLabel style={formStyle} value="yes2" control={<Radio />} label="More than usual" />
+                    <FormControlLabel style={formStyle} value="heck yes2" control={<Radio />} label="Completely" />
 
                 </RadioGroup>
             </FormControl>
 
+                <div>
+
+                <FormControl className="form"  component="fieldset">
+                    <FormLabel style={qStyle} component="legend"> How often have you experienced the feeling of despair in the last week?</FormLabel>
+                    <RadioGroup aria-label="q2" name="question2" value={value} onChange={handleChange}>
+                        <FormControlLabel style={formStyle} value="heck no3" control={<Radio />} label="Not al all" />
+                        <FormControlLabel style={formStyle} value="no3" control={<Radio />} label="A little" />
+                        <FormControlLabel style={formStyle} value="maybe3" control={<Radio />} label="Quite a bit" />
+                        <FormControlLabel style={formStyle} value="yes3" control={<Radio />} label="More than usual" />
+                        <FormControlLabel style={formStyle} value="heck yes3" control={<Radio />} label="Completely" />
+
+                    </RadioGroup>
+                </FormControl>
+                </div>
+
+                <div>
+                <FormControl className="form"  component="fieldset">
+                    <FormLabel style={qStyle} component="legend"> How often would say experienced joy?</FormLabel>
+                    <RadioGroup aria-label="q2" name="question2" value={value} onChange={handleChange}>
+                        <FormControlLabel style={formStyle} value="heck no4" control={<Radio />} label="Not al all" />
+                        <FormControlLabel style={formStyle} value="no4" control={<Radio />} label="A little" />
+                        <FormControlLabel style={formStyle} value="maybe4" control={<Radio />} label="Quite a bit" />
+                        <FormControlLabel style={formStyle} value="yes4" control={<Radio />} label="More than usual" />
+                        <FormControlLabel style={formStyle} value="heck yes4" control={<Radio />} label="Completely" />
+
+                    </RadioGroup>
+                </FormControl>
+            </div>
+
+                <div>
+
+                    <FormControl className="form"  component="fieldset">
+                        <FormLabel style={qStyle} component="legend">What do you look forward to next week?</FormLabel>
+                        <RadioGroup aria-label="q2" name="question2" value={value} onChange={handleChange}>
+                            <FormControlLabel style={formStyle} value="heck no5" control={<Radio />} label="Not al all" />
+                            <FormControlLabel style={formStyle} value="no5" control={<Radio />} label="A little" />
+                            <FormControlLabel style={formStyle} value="maybe5" control={<Radio />} label="Quite a bit" />
+                            <FormControlLabel style={formStyle} value="yes5" control={<Radio />} label="More than usual" />
+                            <FormControlLabel style={formStyle} value="heck yes5" control={<Radio />} label="Completely" />
+
+                        </RadioGroup>
+                    </FormControl>
+
+
+                </div>
+
+
+
             <div className="form">
                 <FormControl component="fieldset">
-                    <FormLabel className="moodText2" component="legend">What do you think could assist in reducing your stress levels?</FormLabel>
+                    <FormLabel className="moodText2" component="legend">What steps could your superiors take to relieve or decrease your work-related stress?</FormLabel>
 
                 </FormControl>
-                <textarea className="commentBox" name="comment">Enter text here</textarea>
+                <textarea className="commentBox3" name="comment">Enter text here</textarea>
 
             </div>
 
+                <div className="form">
+                    <FormControl component="fieldset">
+                        <FormLabel className="moodText2" component="legend">What song would you say describes your mood this week?</FormLabel>
+
+                    </FormControl>
+                    <textarea className="commentBox" name="comment">Enter text here</textarea>
+
+                </div>
+
+                <div className="form">
+                    <FormControl component="fieldset">
+                        <FormLabel className="moodText2" component="legend"> What do you look forward to next week? Any further comments?
+                        </FormLabel>
+
+                    </FormControl>
+                    <textarea className="commentBox1" name="comment">Enter text here</textarea>
+
+                </div>
+
+                <div className="form">
+                    <FormControl component="fieldset">
+                        <FormLabel className="moodText2" component="legend"> Overall would you say this was successful work week?</FormLabel>
+
+                    </FormControl>
+                    <textarea className="commentBox2" name="comment">Enter text here</textarea>
+
+                </div>
+
             <div className="form">
 
-                <h2 className="moodText1">How do you feel overall this week?</h2>
+                <h2 className="moodText1">What would you say is your overall mood for the week?</h2>
                 <img  className="moodImg" src={happy} alt="happy"/>
                 <img className="moodImg" src={angry} alt="angry"/>
                 <img className="moodImg" src={sad} alt="sad"/>
